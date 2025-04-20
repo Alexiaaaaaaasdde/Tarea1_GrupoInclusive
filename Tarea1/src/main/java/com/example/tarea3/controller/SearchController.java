16c9249b53a24728c0f5dc6f412725bacf02c9e4
AlexiaPT <153009848+Alexiaaaaaaasdde@users.noreply.github.com>
2025-04-20 13:26:33 -0500

    add searchController

--- Tarea1/src/main/java/com/example/tarea3/controller/SearchController.java
+++ Tarea1/src/main/java/com/example/tarea3/controller/SearchController.java
@@ -1,4 +1,20 @@
 package com.example.tarea3.controller;
 
+@Controller
+@RequestMapping("/search")
 public class SearchController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public String indiceReportes() {
+        return "search/indice";
+    }
+
+    @GetMapping("/salarios-altos")
+    public String empleadosConAltoSalario(Model model) {
+        List<Employee> empleados = employeeRepository.findBySalaryGreaterThan(15000);
+        model.addAttribute("empleados", empleados);
+        return "search/salarios_altos";
+    }
 }
